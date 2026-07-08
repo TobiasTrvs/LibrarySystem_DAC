0a5704d35192613f7e77cccd257a105f8757f613
Tobias Trevisan <tobiastrevisan80@gmail.com>
2026-07-07 21:06:33 -0300

    mudanças nos imports das entidades

--- library/src/main/java/com/library_system/library/Repository/ExemplarRepository.java
+++ library/src/main/java/com/library_system/library/Repository/ExemplarRepository.java
@@ -2,8 +2,6 @@ package com.library_system.library.Repository;
 import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
-
-import com.library_system.library.dto.exemplar.ExemplarRequestDTO;
 import com.library_system.library.entity.Exemplar;
 import com.library_system.library.entity.StatusExemplar;
 @Repository
